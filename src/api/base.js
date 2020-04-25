9740ee3d0ee020fa8af91e809b32ee51c99068ae
gooingyu <gooingyu@outlook.com>
2020-04-25 16:28:12 +0800

    fix requset head token state

--- src/api/base.js
+++ src/api/base.js
@@ -1,16 +1,18 @@
+/*
+ * @Author: gooing
+ * @since: 2020-02-02 14:59:46
+ * @lastTime: 2020-04-25 16:27:00
+ * @LastAuthor: gooing
+ * @FilePath: \pixiciv-pc\src\api\base.js
+ * @message:
+ */
 import axios from 'axios';
 import cookie from 'js-cookie';
-import router from '../router';
 
 // 创建axios实例
 const instance = axios.create({
   baseURL: 'https://api.pixivic.com',
-  // baseURL: 'https://v1.api.pixivic.com',
-  // baseURL: "https://search.api.pixivic.com",
-  // baseURL: 'http://114.67.107.177:8080',
-  headers: {
-    // 'Authorization': 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJwZXJtaXNzaW9uTGV2ZWwiOjEsInJlZnJlc2hDb3VudCI6MSwiaXNCYW4iOjEsInVzZXJJZCI6MTAsImlhdCI6MTU3MTQ4MzE3MywiZXhwIjoxNTczMjExMTczfQ.PPip1ncfMtUhYMPFOk7ZNln7pPJ1Sb4UMri6VYtbB9FcQscaCe-nevmCM2IWPT7B35Wf6x2cnUjfMSUl86dstg'
-  },
+  // baseURL: process.env.VUE_APP_BASE_API,
   timeout: 20000,
   validateStatus(status) {
     return status >= 200 && status < 600;
@@ -41,8 +43,8 @@ instance.interceptors.response.use(
     if (response.status === 401) {
       // 登录过期
       cookie.remove('jwt');
-      localStorage.remove('user');
-      router.push('/login');
+      localStorage.removeItem('user');
+      window.location.href = '/';
     }
     return response;
   },
