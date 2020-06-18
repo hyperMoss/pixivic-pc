ed2ec37bf854dded571c0b9e26aced598c015b05
gooingyu <gooingyu@outlook.com>
2020-06-18 18:05:44 +0800

    save

--- src/components/VirtualList/index.vue
+++ src/components/VirtualList/index.vue
@@ -8,21 +8,156 @@
 --> 
 
 <template>
-  <div class="index"></div>
+  <div class="index">
+    <div
+      class="container"
+      ref="container"
+    />
+  </div>
 </template>
 
 <script>
+import mixin from './mixin';
+import { getScrollTop, getScrollHeight, getWindowHeight } from './tool';
 export default {
+  mixins: [mixin],
   name: 'VirtualList',
   data() {
-    return {
-    };
+    return {};
   },
   computed: {},
   components: {},
   watch: {},
-  methods: {},
-  mounted() {},
+  methods: {
+    init() {
+      //获取组件父级宽
+      const contWidth = this.$refs['container'].clientWidth;
+      //通过组件宽获取列宽
+      this.setColumnsWidth(contWidth);
+
+      //获取可视区域高度
+      this.setWindowHeight(document.documentElement.clientHeight);
+
+      //获取left坐标
+      this.setlocationLeft();
+
+      //定位初始化top值
+      this.setInitTop(this.maxcolumns);
+
+      this.getNewImg().then(() => (window.onscroll = () => this.scrollAction()));
+    },
+    //新资源
+    getNewImg(getNoShowRobotListNum) {
+      return this.getNewImgData(this.requestImgNum).then(res => {
+        for (let i = 0; i < res.data.length; i++) {
+          //获取机器人id
+          let robotId = this.generatorId({ i, getNoShowRobotListNum });
+          let beyond = this.positioning(res.data[i], robotId);
+          if (beyond) {
+            break;
+          }
+        }
+      });
+    },
+    /**
+     * 增加的时候开始 定位
+     */
+    positioning(element, robotId) {
+      //屏幕总数
+      let screenAllNum = this.screenAllNum;
+      //定位与列数一直时 换行
+      this.state.positioningNum == this.maxcolumns ? (this.state.positioningNum = 0) : null;
+      //图片原始宽度
+      const imgWidth = element.width;
+      //列宽限定
+      const columnsWidth = this.columnsWidth;
+      //计算出图片在进入dom后的高度
+      const imgHeight = (columnsWidth / imgWidth) * element.height;
+      element.domHeight = imgHeight;
+      //宽
+      element.domWidth = columnsWidth;
+      const robotTopLocation = this.robotTopLocation;
+      //获取top数组中最小的座位下标
+      const minTopNum = robotTopLocation.indexOf(Math.min.apply(Math, robotTopLocation));
+      //获取top值
+      element.top = robotTopLocation[minTopNum];
+      //获取left值
+      element.left = this.locationLeft[minTopNum];
+      //位列一行中第几位
+      element.columnsNum = minTopNum;
+      //设定下一个同列图片的top
+      this.setTop(minTopNum, imgHeight);
+      const topNum = this.robotTopLocation[minTopNum];
+      this.upContainerHeight(topNum);
+      //获取所放置的屏幕号 下标
+      let srceenIng = screenAllNum - 1;
+      //创建
+      this.addlocationInfo(element, robotId, srceenIng);
+      //增长
+      this.state.positioningNum++;
+      //top大于当前屏幕高度 则增加屏幕
+      if (element.top > this.windowViewHeight * screenAllNum) {
+        //超出高度 则不管了
+        return 'beyond';
+      }
+    },
+
+    scrollAction() {
+      const windowHeight = getWindowHeight();
+
+      const scrollTop = getScrollTop() + windowHeight;
+
+      const screenNumIng = Math.floor(scrollTop / windowHeight) - 1;
+
+      if (screenNumIng !== this.numIng) {
+        this.changeScreen(screenNumIng);
+      }
+
+      if (scrollTop === getScrollHeight()) {
+        //增加下一个屏幕的元素队列
+        this.addlocationInfoNum(this.screenAllNum);
+
+        //增加屏幕
+        this.addScreen();
+
+        //增加当前加载的屏幕队
+        this.addloadingScreen(this.screenAllNum);
+
+        //获取未显示的机器人屏幕下标
+        const getNoShowRobotListNum = this.screenAllNum - 4;
+
+        this.getNewImg(getNoShowRobotListNum < 1 ? undefined : getNoShowRobotListNum - 1);
+      }
+    },
+    //随机id
+    performanceId() {
+      return performance
+        .now()
+        .toString()
+        .substring(0, 6);
+    },
+
+    /**
+     * 生成机器人id
+     */
+    generatorId(getInfo) {
+      //没有给已知下标，则新建
+      if (!getInfo.getNoShowRobotListNum && getInfo.getNoShowRobotListNum != 0) {
+        return this.performanceId();
+      }
+
+      const getNoShowRobotListNum = getInfo.getNoShowRobotListNum.toString();
+
+      const i = getInfo.i.toString();
+
+      const domInfo = this.locationInfo([getNoShowRobotListNum, i]);
+
+      return domInfo ? domInfo.robotid : this.performanceId();
+    },
+  },
+  mounted() {
+    this.init();
+  },
 };
 </script>
 
--- src/components/VirtualList/mixin.js
+++ src/components/VirtualList/mixin.js
@@ -37,9 +37,10 @@ export default {
        * value 元素队列
        */
       locationInfo: {
-        ["0"]: []
+        "0": []
       },
 
+      robotContainer: [],
       /**
        * 机器人的top位置，每次排列一排之后完成后，根据这一排的信息获取下一排的top
        * null为初始状态，根据父级组件完成渲染后而改变
@@ -51,7 +52,7 @@ export default {
        */
 
       //屏幕总数量 个数
-      allNum: 1,
+      screenAllNum: 1,
       //当前第几屏幕 下标
       numIng: 0,
       //上一次所在屏幕 下标
@@ -65,7 +66,7 @@ export default {
     /**
      * 通过父级组件的宽获取列宽
      */
-    setColumsWidth(width) {
+    setColumnsWidth(width) {
       this.columnsWidth = Math.floor(
         (width - this.spacing * this.maxcolumns) / this.maxcolumns
       );
@@ -75,7 +76,7 @@ export default {
      */
     setLocationLeft() {
       this.columnsWidth += this.spacing;
-      let newLeftLocation = [];
+      const newLeftLocation = [];
       for (let i = 0; i < this.maxColumns; i++) {
         newLeftLocation.push(this.columnsWidth * i + this.spacing / 2);
       }
@@ -94,31 +95,127 @@ export default {
       this.containerHeight = Math.max(this.containerHeight, height);
     },
 
-
-
-
-
     /**
      * 增加屏幕
      */
-    addScreen(){
-      this.allNum++
+    addScreen() {
+      this.screenAllNum++;
     },
     /**
      * 增加当前加载的屏幕队
      */
-    addLoadingScreen(allNum){
-      let subscript =allNum-1
+    addLoadingScreen(screenAllNum) {
+      const subscript = screenAllNum - 1;
 
       //最多显示4个屏幕
       for (let i = 0; i < 4; i++) {
-        let showScreen = subscript - i;
+        const showScreen = subscript - i;
         if (showScreen < 0) {
           break;
         }
-        this.loadingScreenList = this.loadingScreenList.set(i, showScreen);
+        // 用数组变异方法给当前屏幕加载队列赋值
+        this.loadingScreen.splice(i, 1, showScreen);
       }
       //返回新的显示数组
-     
+      return this.loadingScreen;
+    },
+    /**
+     * 更新上一次的屏幕号
+     */
+    updataLoadingScreen(data) {
+      this.oldNum = data;
+    },
+    /**
+     * 更新现在的屏幕号
+     */
+    updateChangeScreen(data) {
+      this.numIng = data;
+    },
+    /**
+     * 下滑改变屏幕队列
+     */
+    changeDownChange(data) {
+      this.loadingScreen.splice(0, data - 1);
+      this.loadingScreen.splice(1, data);
+      this.loadingScreen.splice(2, data + 1);
+      this.loadingScreen.splice(3, data + 2);
+      return this.loadingScreen;
+    },
+
+    /**
+     * 上滑改变屏幕队列
+     */
+    changeUpChange(data) {
+      this.loadingScreen.splice(0, data - 2);
+      this.loadingScreen.splice(1, data - 1);
+      this.loadingScreen.splice(2, data);
+      this.loadingScreen.splice(3, data + 1);
+      return this.loadingScreen;
+    },
+
+    /**
+     * 机器人部分函数
+     */
+
+    /**
+     * 添加机器人
+     */
+    addlocationInfo(data) {
+      const { robotId, screenNumIng, imgData } = data;
+      const {
+        src,
+        width,
+        height,
+        left,
+        top,
+        domHeight,
+        domWidth,
+        columnsNum
+      } = imgData;
+      this.locationInfo[screenNumIng.toString()].push({
+        robotId: robotId,
+        img: src,
+        owidth: width,
+        oheight: height,
+        left: left,
+        top: top,
+        domHeight: domHeight,
+        domWidth: domWidth,
+        columnsNum: columnsNum
+      });
+    },
+    /**
+     * 容器存储机器人
+     */
+    setRobotContainer(data) {
+      const getRobot = data.map(item => {
+        return { robotId: item.robotId, positionInfo: item };
+      });
+      return this.robotContainer.concat(getRobot);
+    },
+    /**
+     * 初始化top
+     */
+    initTop(data) {
+      this.robotTopLocation = new Array(data).fill(0);
+    },
+    /**
+     * 更新top值
+     */
+    setTop(columnsNum, height) {
+      this.robotTopLocation.splice(
+        columnsNum,
+        1,
+        this.robotTopLocation[columnsNum] + height
+      );
+      return this.robotTopLocation;
+    },
+    /**
+     * 添加位置信息
+     */
+    addLocationInfoNum(data) {
+      this.$set(this.locationInfo, data.toString(), []);
+      return this.locationInfo;
+    }
   }
 };
