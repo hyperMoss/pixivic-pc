e44ced3d6719970a9bfea8d52a1d9c8aa21bc623
gooingyu <gooingyu@outlook.com>
2020-06-23 21:41:56 +0800

    :sparkles: add emailcheck
    add email check

---
+++ src/views/User/Setting/MailCheck.vue
@@ -0,0 +1,35 @@
+<!--
+ * @Author: Dongzy
+ * @since: 2020-06-23 21:20:50
+ * @lastTime: 2020-06-23 21:40:21
+ * @LastAuthor: Dongzy
+ * @FilePath: \pixiciv-pc\src\views\User\Setting\MailCheck.vue
+ * @message:
+-->
+<template>
+  <div />
+</template>
+
+<script>
+export default {
+  name: 'MailCheck',
+  data() {
+    return {
+
+    };
+  },
+  mounted() { this.checkEmail(); },
+  methods: {
+    checkEmail() {
+      const query = this.$route.query;
+      delete query.VNK;
+      this.$api.user.setEmail(query)
+        .then(res => {
+          alert(res.data.message);
+          setTimeout(() => { window.location.href = '/'; }, 1000);
+        });
+    }
+  },
+};
+</script>
+
--- src/views/User/index.js
+++ src/views/User/index.js
@@ -1,14 +1,15 @@
 /*
  * @Author: gooing
  * @since: 2020-03-23 23:14:54
- * @lastTime: 2020-06-07 23:37:24
- * @LastAuthor: gooing
+ * @lastTime: 2020-06-23 21:41:08
+ * @LastAuthor: Dongzy
  * @FilePath: \pixiciv-pc\src\views\User\index.js
  * @message:
  */
 import BookMarked from './BookMarked/index.vue';
 import Followed from './Followed/index.vue';
 import HomePage from './HomePage/index.vue';
+import MailCheck from './Setting/MailCheck.vue';
 
 export default [
   {
@@ -26,5 +27,10 @@ export default [
     name: 'HomePage',
     component: HomePage,
     props: true
+  },
+  {
+    path: '/emailCheck',
+    name: 'MailCheck',
+    component: MailCheck,
   }
 ];
