4e9b64281c2e62e783ca517a6311946f184a3b82
dongzheyu <dongzheyu@shein.com>
2020-08-17 19:49:46 +0800

    fix

--- src/components/PublicComponents/Comment.vue
+++ src/components/PublicComponents/Comment.vue
@@ -1,4 +1,3 @@
-
 <template>
   <div class="container">
     <div
@@ -40,7 +39,8 @@
             @click="likeClick(item)"
           >
             <i class="iconfont icon-like" />
-            <span class="like-num"> <i class="el-icon-caret-top" /> {{ item.likedCount > 0 ? item.likedCount + '人赞' : '赞' }}</span>
+            <span class="like-num"> <i class="el-icon-caret-top" />
+              {{ item.likedCount > 0 ? item.likedCount + '人赞' : '赞' }}</span>
           </span>
           <span
             class="comment-reply"
@@ -158,6 +158,7 @@
 
 <script>
 import { mapGetters } from 'vuex';
+
 export default {
   components: {},
   props: {
@@ -176,9 +177,9 @@ export default {
       copyComment: '',
       inputComment: '',
       showItemId: '',
-      total:0,
+      total: 0,
       pageSize: 20,
-      pageIndex:1,
+      pageIndex: 1,
     };
   },
   computed: {
@@ -197,13 +198,13 @@ export default {
       this.$api.comment.getComments({
         commentAppType: this.$props.commentType,
         commentAppId: this.pid,
-        pageSize:10,
-        page:this.pageIndex
+        pageSize: 10,
+        page: this.pageIndex
       })
         .then(res => {
           if (res.status === 200) {
             this.comments = res.data.data || [];
-            this.total=res.data.total;
+            this.total = res.data.total || 0;
           }
         });
     },
@@ -285,6 +286,7 @@ export default {
             const params = { ...data, createDate: new Date(), replyFrom: this.user.id, id: Math.random() };
             if (params.parentId === 0) {
               this.comments.push(params);
+              this.total++;
             } else {
               const item = this.comments.find(item => item.id === params.parentId);
               if (item.subCommentList) {
@@ -335,6 +337,7 @@ export default {
   width: 100%;
   padding: 0 10px;
   box-sizing: border-box;
+
   .write-reply {
     display: flex;
     align-items: center;
@@ -342,149 +345,182 @@ export default {
     color: @text-minor;
     padding: 10px;
     cursor: pointer;
+
     &:hover {
       color: @text-main;
     }
+
     .el-icon-edit {
       margin-right: 5px;
     }
   }
+
   .comment {
     display: flex;
     flex-direction: column;
     padding: 10px;
     border-bottom: 1px solid @border-fourth;
+
     .info {
       display: flex;
       align-items: center;
+
       .avatar {
         border-radius: 50%;
       }
+
       .right {
         display: flex;
         flex-direction: column;
         margin-left: 10px;
+
         .name {
           font-size: 16px;
           color: @text-main;
           margin-bottom: 5px;
           font-weight: 500;
         }
+
         .date {
           font-size: 12px;
           color: @text-minor;
         }
       }
     }
+
     .content {
       font-size: 16px;
       color: @text-main;
       line-height: 20px;
       padding: 10px 0;
     }
+
     .control {
       display: flex;
       align-items: center;
       font-size: 14px;
       color: @text-minor;
+
       .like {
         display: flex;
         align-items: center;
         margin-right: 20px;
         cursor: pointer;
+
         &.active,
         &:hover {
           color: @color-main;
         }
+
         .iconfont {
           font-size: 14px;
           margin-right: 5px;
         }
       }
+
       .comment-reply {
         display: flex;
         align-items: center;
         cursor: pointer;
+
         &:hover {
           color: @text-333;
         }
+
         .iconfont {
           font-size: 16px;
           margin-right: 5px;
         }
       }
     }
+
     .reply {
       margin: 10px 44px;
       border-left: 2px solid @border-first;
+
       .comment-item {
         margin: 0 10px;
         padding: 10px 0;
         border-bottom: 1px dashed @border-third;
+
         .reply-content {
           display: flex;
           align-items: center;
           font-size: 14px;
           color: @text-main;
+
           .from-name {
             color: @color-main;
           }
+
           .to-name {
             color: @color-main;
             margin-left: 5px;
             margin-right: 5px;
           }
         }
+
         .reply-bottom {
           display: flex;
           align-items: center;
           margin-top: 6px;
           font-size: 12px;
           color: @text-minor;
+
           .reply-text {
             display: flex;
             align-items: center;
             margin-left: 10px;
             cursor: pointer;
+
             &:hover {
               color: @text-333;
             }
+
             .icon-comment {
               margin-right: 5px;
             }
           }
         }
       }
+
       .fade-enter-active,
       fade-leave-active {
         transition: opacity 0.5s;
       }
+
       .fade-enter,
       .fade-leave-to {
         opacity: 0;
       }
     }
   }
+
   .input-wrapper {
     padding: 10px;
+
     .gray-bg-input,
     .el-input__inner {
       /*background-color: #67C23A;*/
     }
+
     .btn-control {
       display: flex;
       justify-content: flex-end;
       align-items: center;
       padding-top: 10px;
+
       .cancel {
         font-size: 16px;
         color: @text-normal;
         margin-right: 20px;
         cursor: pointer;
+
         &:hover {
           color: @text-333;
         }
       }
+
       .confirm {
         font-size: 16px;
       }
